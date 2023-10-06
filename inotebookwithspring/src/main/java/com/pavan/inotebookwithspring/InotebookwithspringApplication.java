1c01aa4cbaafeb70b7d870319e11446e1aaf8ddf
Pavandkurdekar <pavandkurdekar55@gmail.com>
2023-10-06 19:21:49 +0530

    First commit

---
+++ inotebookwithspring/src/main/java/com/pavan/inotebookwithspring/InotebookwithspringApplication.java
@@ -0,0 +1,13 @@
+package com.pavan.inotebookwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InotebookwithspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InotebookwithspringApplication.class, args);
+	}
+
+}
---
+++ inotebookwithspring/src/test/java/com/pavan/inotebookwithspring/InotebookwithspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavan.inotebookwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InotebookwithspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
