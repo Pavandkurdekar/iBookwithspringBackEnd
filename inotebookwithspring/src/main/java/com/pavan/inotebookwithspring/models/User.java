1121cb3a5ac760945549b62729ccb8c636d11222
Pavandkurdekar <pavandkurdekar55@gmail.com>
2023-10-06 19:52:42 +0530

    Second Commit

---
+++ inotebookwithspring/src/main/java/com/pavan/inotebookwithspring/models/User.java
@@ -0,0 +1,57 @@
+package com.pavan.inotebookwithspring.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Users")
+public class User
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(nullable = false,unique = true)
+	private String name;
+	
+	
+	@Column(nullable = false)
+	private String password;
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+}
